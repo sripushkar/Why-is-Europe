72f4dfd184861d3dc690d314f1828a7980b1d1a0
sripushkar <sripushkar@gmail.com>
2020-01-13 05:31:04 -0500

    fixed line spacing

--- main.py
+++ main.py
@@ -1,21 +1,16 @@
 import requests
 
-
 with open("euroCountries.txt") as f:
   countriesList = f.readlines()
 
 countriesList = [line.rstrip('\n') for line in open("euroCountries.txt")]
-
 listLength = len(countriesList)
 
 i = 0
-
 while i < listLength:
     query = "Why+is+" + countriesList[i] + "+so+"
 
     results = requests.get("http://suggestqueries.google.com/complete/search?q="+query+"&client=firefox&hl=en")
-
     resultsJSON = results.json()
-
     print(resultsJSON[1][0])
     i+=1
